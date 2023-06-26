5740f732f53edc5ed0b1b26f8df0c04a11556a3c
Steve Magana <steveamagana@gmail.com>
2023-06-26 02:40:16 -0700

    Rename posts route to feed route and create header/footer partials

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const flash = require("express-flash");
 const logger = require("morgan");
 const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
-const postRoutes = require("./routes/posts");
+const feedRoutes = require("./routes/feed");
 const commentRoutes = require("./routes/comments");
 
 //Use .env file in config folder
@@ -56,7 +56,7 @@ app.use(flash());
 
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
-app.use("/post", postRoutes);
+app.use("/feed", feedRoutes);
 app.use("/comment", commentRoutes);
 
 //Server Running
